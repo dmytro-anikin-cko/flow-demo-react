692869410ed7e9bcc948f1c1870e4236edca0178
Dmytro Anikin <dmytro.anikin@checkout.com>
2024-08-13 12:54:51 +0200

    Handling Webhooks Fix

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,21 +1,14 @@
 import { NextResponse } from "next/server";
-import { buffer } from 'micro';
-import crypto from 'crypto';
-
-export const config = {
-  api: {
-    bodyParser: false, // We disable the body parser to handle raw body
-  },
-};
+import crypto from "crypto";
 
 export async function POST(req, res) {
   try {
     // Get the raw body as a buffer
-    const buf = await buffer(req);
-    const rawBody = buf.toString("utf8");
+    const rawBody = await req.text();
 
-    // Validate the signature (if provided by Checkout.com)
-    const signature = req.headers["cko-signature"];
+    // Extract the signature from the headers
+    const signature = req.headers.get("cko-signature");
+
     const secret = process.env.WEBHOOK_SIGNATURE_KEY; // Your secret key from Checkout.com
 
     const hash = crypto
@@ -24,21 +17,28 @@ export async function POST(req, res) {
       .digest("hex");
 
     if (hash !== signature) {
-      console.error('Invalid signature. Possible tampering detected.');
-      return NextResponse.json({ received: false, message: "Invalid signature" }, { status: 400 });
+      console.error("Invalid signature. Possible tampering detected.");
+      return NextResponse.json(
+        { received: false, message: "Invalid signature" },
+        { status: 400 }
+      );
     }
 
     // Parse the raw body
     const event = JSON.parse(rawBody);
-
+    console.log(event);
+    
     // Handle the webhook event
     if (event.type === "payment_approved") {
       console.log("Message from CKO: payment_approved");
-    }
+    } 
 
     // Return a response to acknowledge receipt of the webhook
     // return res.status(200).json({ received: true });
-    return NextResponse.json({ received: true, message: "CKO payment approved" }, { status: 200 });
+    return NextResponse.json(
+      { received: true, message: "CKO payment approved" },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error processing webhook:", error);
     return NextResponse.json({ error: error.body }, { status: 500 });
